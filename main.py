293bc62cdaa1d8c2ac4b0843036701fb70dd3596
blakecoppens <blakecoppens004@gmail.com>
2023-10-28 00:08:07 -0400

    Updated and added comments

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ def encode(password):
     for i in encoded_list:
         encoded_pw += i[-1]
     return encoded_pw
-# testing if I can commit and push
-def decode(encoded_pw):
+
+def decode(encoded_pw1): # Decoder Function -- Blake Coppens
     
-    if len(encoded_pw) > 8:
+    if len(encoded_pw1) > 8:
         print("Error! User password is longer than 8 digits.\nShortening to 8 digits...")
-        encoded_pw = encoded_pw[:8]
+        encoded_pw = encoded_pw1[:8] #shortens the length
     decoded_pw = ''
-    for i in encoded_pw:
-        convertednum = int(i)-3
+    for i in encoded_pw1: 
+        convertednum = int(i)-3 #Makes sure that the length stays at most 8 digits.
         if convertednum <0:
             if convertednum == -3:
                 convertednum = 7
@@ -31,14 +31,14 @@ def decode(encoded_pw):
         
     
 
-def menu():
+def menu(): #Menu function
     print('Menu')
     print('-------------')
     print('1. Encode')
     print('2. Decode')
     print('3. Quit')
 
-def main():
+def main(): #Main function
     while True:
         menu()
         option = int(input('Please enter an option: '))
@@ -53,5 +53,5 @@ def main():
             break
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': #Unit test
     main()
